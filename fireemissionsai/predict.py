6da3b190ecbb7bc7445b37bb65139f9c2ae7eabf
rudikershaw <alexander_kershaw@hotmail.co.uk>
2018-01-19 23:48:09 +0000

    Rudi Kershaw - Add example predictions to output to help with super high bias issue.

--- fireemissionsai/predict.py
+++ fireemissionsai/predict.py
@@ -1,5 +1,6 @@
 import keras
 import numpy as np
+import tensorflow as tf
 from keras.utils import plot_model
 from keras.models import Sequential
 from keras.layers import Dense, Activation
@@ -11,14 +12,16 @@ features_test = np.genfromtxt('output/test-features.csv', delimiter=',')
 targets_test = np.genfromtxt('output/test-targets.csv', delimiter=',')
 
 model = Sequential()
-model.add(Dense(units=116, activation='sigmoid', input_dim=227))
+model.add(Dense(units=116, activation='sigmoid', input_dim=features_train.shape[1]))
 model.add(Dense(units=116, activation='sigmoid'))
-model.add(Dense(units=6))
+model.add(Dense(units=targets_train.shape[1]))
 
-model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
+model.compile(loss='mean_absolute_error', optimizer='sgd', metrics=['accuracy'])
 model.fit(
     features_train,
     targets_train,
-    epochs=10,
+    epochs=2,
     validation_data=(features_test, targets_test)
 )
+
+print(model.predict(features_test))
