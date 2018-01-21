63a792a377a59788b87fb33152f56cc381fd0d4b
rudikershaw <alexander_kershaw@hotmail.co.uk>
2018-01-21 14:20:16 +0000

    Rudi Kershaw - Print test predictions to .csv

--- fireemissionsai/predict.py
+++ fireemissionsai/predict.py
@@ -9,9 +9,9 @@ features_test = np.genfromtxt('output/test-features.csv', delimiter=',')
 targets_test = np.genfromtxt('output/test-targets.csv', delimiter=',')
 
 model = keras.models.Sequential()
-model.add(Dense(units=112, input_dim=features_train.shape[1]))
+model.add(Dense(units=112, activation='relu', input_dim=features_train.shape[1]))
 model.add(BatchNormalization())
-model.add(Dense(units=112))
+model.add(Dense(units=112, activation='relu'))
 model.add(Dense(units=targets_train.shape[1], activation='relu'))
 
 sgd = keras.optimizers.SGD(lr=0.01, momentum=0.4, decay=1e-6)
@@ -19,14 +19,17 @@ model.compile(loss='mean_absolute_error', optimizer=sgd, metrics=['accuracy'])
 model.fit(
     features_train,
     targets_train,
-    epochs=10,
+    epochs=20,
     validation_data=(features_test, targets_test)
 )
 
 print("\nPredicted: ")
-print(model.predict(features_test))
+predictions = model.predict(features_test)
+print(predictions)
 
 print("\nExpected: ")
 print(targets_test[0:3])
 print("...,")
 print(targets_test[len(targets_test)-3:len(targets_test)])
+
+np.savetxt('output/test-predictions.csv', predictions, delimiter=',')
