4b7f0fdc72548cdfeafbf3cca5453b7e9c85c98b
rudikershaw <alexander_kershaw@hotmail.co.uk>
2018-02-24 23:08:59 +0000

    Rudi Kershaw - Update predictor to use pre-trained model.

--- README.md
+++ README.md
@@ -1,19 +1,30 @@
 # Fire Emissions AI
 
-Fire Emissions AI is an [artificial neural network](https://en.wikipedia.org/wiki/Artificial_neural_network) designed to predict fire emissions information based on previously collected data for an area. Fire Emmissions AI is trained using freely available data provided by the NASA EarthData [Global Fire Emissions Database V4.1](https://daac.ornl.gov/cgi-bin/dsviewer.pl?ds_id=1293)<sup>1</sup>.
+Fire Emissions AI is an [artificial neural network](https://en.wikipedia.org/wiki/Artificial_neural_network) designed to predict fire emissions information based on previously collected data for an area. Fire Emmissions AI is trained using freely available data provided by the NASA EarthData [Global Fire Emissions Database (GFED) V4.1](https://daac.ornl.gov/cgi-bin/dsviewer.pl?ds_id=1293)<sup>1</sup>.
 
 # Quick Start Guide
+
+First you will need a `.csv` file with lines in the following format. Each value should be extracted directly from the GFED to ensure the correct format of individual values.
+
+```
+[Year],[Month],[Latitude],[Longitude],[Region ID],[BB],[NPP],[Rh],[C],[DM],[Burned Area]
+```
 
 1. You will need [Python 3.5](https://www.python.org/downloads/) or greater installed.
 2. Install Pipenv, if you do not already have it, with `$ pip install pipenv`.
 3. Install dependencies for the project using `$ pipenv install`
 4. Run the unit tests and linters to ensure correct behavior `$ pipenv run python setup.py test`.
-5. Run the preprocess.py with `$ pipenv run python fireemissionsai/preprocess.py [directory containing hdfs]`.
-6. Run the predict.py with `$ pipenv run python fireemissionsai/predict.py`
+5. Run the predict.py with `$ pipenv run python fireemissionsai/predict.py [path to .csv]`.
 
-The repository contains two main scripts; `fireemissionsai/predict.py` and `fireemissionsai/preprocess.py`. The former contains the code relating to the neural network directly (including training, testing, and eventually predicting), the latter contains the code for a utility used to convert the Global Fire Emissions Database files into training examples for the predictor. The preprocess.py outputs .csv files that are used for training the predictor.
+The repository contains two main scripts; `fireemissionsai/predict.py` and `fireemissionsai/preprocess.py`. The former contains the code relating to the neural network directly (including training, testing, and predicting), the latter contains the code for a utility used to convert the Global Fire Emissions Database files into training examples for the predictor. The preprocess.py outputs `.csv` files that are used for training the predictor.
 
-In a future release, the predictor will be set up to use a pre-trained model and act as a means of predicting future values from a .csv containing values to make predictions from. Currently the predict.py script only trains a model on provided data and outputs accuracy and loss values and prints a file with some example predictions.
+To train the predictor on your own data simply do the following.
+
+1. Run the preprocess.py with `$ pipenv run python fireemissionsai/preprocess.py [directory containing GFED hdfs]`.
+2. Run the predict.py with `$ pipenv run python fireemissionsai/predict.py [path to .csv] --retrain --persist`.
+
+Using the `--persist` flag will save your newly trained model and weights over the existing model. To train a model for one-time-use simply omit the `--persist` flag.
+
 
 # References
 
--- fireemissionsai/predict.py
+++ fireemissionsai/predict.py
@@ -1,23 +1,35 @@
-"""The predict module is for creating and training a model, and eventually prediction."""
+"""The predict module is for taking a .csv of month specific fire emissions features
+and predicting the values for the following month. All the predict.py command requires
+path to such a .csv file.
 
+Optionally providing a --retrain parameter will have the predict.py command attempt to
+retrain its model on data in the ouput folder (to get data for training, use the
+preprocess.py module) before predicting values with the new model. If the --persist
+parameter is also provided the new learned model and its weights will be persisted for
+future use.
+"""
+
+import os
 import keras
 import numpy as np
+from keras.models import load_model
 from keras.layers import Dense, BatchNormalization
+from argparse import ArgumentParser, RawTextHelpFormatter
 
 def construct_model(input_shape, output_shape):
     """Construct the model for predicting next month's fire emissions data."""
     model = keras.models.Sequential()
-    model.add(Dense(units=512, activation='relu', input_dim=input_shape))
+    model.add(Dense(units=412, activation='relu', input_dim=input_shape))
     model.add(BatchNormalization())
-    model.add(Dense(units=512, activation='relu'))
-    model.add(Dense(units=512, activation='relu'))
+    model.add(Dense(units=412, activation='relu'))
+    model.add(Dense(units=412, activation='relu'))
     model.add(Dense(units=output_shape, activation='relu'))
 
     sgd = keras.optimizers.SGD(lr=0.02, momentum=0.8, decay=1e-6)
     model.compile(loss='mean_absolute_error', optimizer=sgd, metrics=['accuracy'])
     return model
 
-def train_validate_test_print(model, train_x, train_y):
+def train_validate_test_print(model, train_x, train_y, input_x, persist):
     """Train a provided model on the provided training set."""
     model.fit(
         train_x,
@@ -44,10 +56,32 @@ def train_validate_test_print(model, train_x, train_y):
     print(test_y[len(test_y)-3:len(test_y)])
 
     np.savetxt('output/test-predictions.csv', predictions, delimiter=',')
+    np.savetxt('output/predictions.csv', model.predict(input_x), delimiter=',')
+    if persist:
+        model.save('model_weights.h5')
 
 if __name__ == "__main__":
-    TRAIN_X = np.genfromtxt('output/train-features.csv', delimiter=',')
-    TRAIN_Y = np.genfromtxt('output/train-targets.csv', delimiter=',')
+    PARSER = ArgumentParser(description=__doc__, formatter_class=RawTextHelpFormatter)
+    PARSER.add_argument("inputs", help="Path to .csv with input features for prediction")
+    PARSER.add_argument("--retrain", dest='retrain', action='store_true', help='Retrain the model')
+    PARSER.add_argument("--persist", dest='persist', action='store_true', help='Save retrained model')
+    PARSER.add_argument("--debug", dest='debug', action='store_true')
+    PARSER.set_defaults(retrain=False)
+    PARSER.set_defaults(persist=False)
+    PARSER.set_defaults(debug=False)
+    ARGS = PARSER.parse_args()
 
-    MODEL = construct_model(TRAIN_X.shape[1], TRAIN_Y.shape[1])
-    train_validate_test_print(MODEL, TRAIN_X, TRAIN_Y)
+    if not ARGS.debug:
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+    INPUT_X = np.genfromtxt(ARGS.inputs, delimiter=',')
+    if ARGS.retrain:
+        TRAIN_X = np.genfromtxt('output/train-features.csv', delimiter=',')
+        TRAIN_Y = np.genfromtxt('output/train-targets.csv', delimiter=',')
+
+        MODEL = construct_model(TRAIN_X.shape[1], TRAIN_Y.shape[1])
+        train_validate_test_print(MODEL, TRAIN_X, TRAIN_Y, INPUT_X, ARGS.persist)
+    else:
+        MODEL = load_model('model_weights.h5')
+        np.savetxt('output/predictions.csv', MODEL.predict(INPUT_X), delimiter=',')
+        print('\nPredictions saved to output directory.\n')
